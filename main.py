d1d023799902f4802d366510bf87b4e5beb00381
PromotezCitizen <gkstkdgus821@gmail.com>
2025-02-10 18:06:56 +0900

    추론 endpoint를 하나로 수정
    - error에 따라 import하는 모델 설정
    - 추론 시 댓글 전처리 과정 추가

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from dotenv import load_dotenv
-
-import torch
 
 import re
 import os
 
-from helpers import ONNXClassificationModel, TransformerClassificationModel, GoogleDriveHelper
+do_not_download_list = ['dataset-backup']
+
+try:
+    raise ValueError("For Testing!!")
+
+    import torch
+    from helpers import TransformerClassificationModel
+
+    if torch.cuda.is_available():
+        comment_model = TransformerClassificationModel(model_type="comment")
+        nickname_model = TransformerClassificationModel(model_type="nickname")
+
+        do_not_download_list.extend(['comment_onnx', 'nickname_onnx'])
+
+        print("Transformer loaded")
+
+        model_type = 'transformer'
+    else:
+        raise ValueError("CUDA not available")
+except Exception as e:
+    print(f"Error message: {str(e)}")
+
+    try:
+        from helpers import ONNXClassificationModel
+
+        comment_model = ONNXClassificationModel(model_type="comment")
+        nickname_model = ONNXClassificationModel(model_type="nickname")
+
+        do_not_download_list.extend(['comment_model', 'nickname_model'])
+
+        print("ONNX loaded")
+
+        model_type = 'onnxruntime-gpu'
+    except ImportError:
+        raise ImportError("Neither TransformerClassificationModel nor ONNXClassificationModel could be imported")
+
+from helpers import GoogleDriveHelper
 
 project_root_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,17 +50,6 @@ load_dotenv(os.path.join(project_root_dir, "env", ".env"))
 google_drive_owner_email = os.getenv("GOOGLE_DRIVE_OWNER_EMAIL")
 do_not_download_list = ['dataset-backup']
 google_client_key_path = os.path.join(project_root_dir, 'env', 'ml-server-key.json')
-
-if torch.cuda.is_available():
-    comment_model = TransformerClassificationModel(model_type="comment")
-    nickname_model = TransformerClassificationModel(model_type="nickname")
-    do_not_download_list.extend(['comment_onnx', 'nickname_onnx'])
-    print("BERT loaded")
-else:
-    comment_model = ONNXClassificationModel(model_type="comment")
-    nickname_model = ONNXClassificationModel(model_type="nickname")
-    do_not_download_list.extend(['comment_model', 'nickname_model'])
-    print("ONNX loaded")
 
 helper = GoogleDriveHelper(project_root_dir=project_root_dir,
                            google_client_key_path=google_client_key_path,
@@ -55,6 +78,7 @@ class PredictRequest(BaseModel):
 
 class PredictResponse(BaseModel):
     items: List[PredictResult]
+    model_type: Optional[str]
 
 async def startup():
     nickname_model.load()
@@ -66,53 +90,31 @@ async def shutdown():
 
 app.add_event_handler("startup", startup)
 app.add_event_handler("shutdown", shutdown)
-
+    
 @app.post("/predict")
-def predict(data: PredictRequest):
-    try:
-        items = data.items
-        response_data = []
-
-        for item in items:
-            comment = item.comment
-            comment = re.sub(r'\d+:(?:\d+:?)?\d+', '[TIME]', comment)
-
-            nickname = item.nickname
-            nickname = re.sub(r"-\w{3}$|-\w{5}$", "", nickname)
-            nickname = re.sub(r'[-._]', ' ', nickname)
-
-            comment_output = comment_model.predict(comment)
-            nickname_output = nickname_model.predict(nickname)
-
-            response_data.append(PredictResult(id=item.id, 
-                                               nickname_predicted=nickname_output,
-                                               comment_predicted=comment_output))
-        
-        # 결과 반환
-        return PredictResponse(items=response_data)
-    
-    except Exception as e:
-        return {
-            'status': 'error',
-            'message': str(e)
-        }
-    
-@app.post("/predict/batch")
 def predict_batch(data: PredictRequest):
     try:
         items = data.items
         response_data = []
 
-        comments = [
-            re.sub(r'chill', '칠', 
-                   re.sub(r'\d+:(?:\d+:?)?\d+', '[TIME]', item.comment), flags=re.IGNORECASE)
-                    for item in items]
-        nicknames = [re.sub(r"-[a-zA-Z0-9]{3}$|-[a-zA-Z0-9]{5}$", '', item.nickname) for item in items]
+        nicknames = [item.nickname.replace('@', '') for item in items]
+        nicknames = [re.sub(r"[^a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9-_.]+", '[DEFAULT_NICK]', nickname) for nickname in nicknames]
+        nicknames = [re.sub(r"-[a-zA-Z0-9]{3}$|-[a-zA-Z0-9]{5}$", '', nickname) for nickname in nicknames]
         nicknames = [re.sub(r'[-._]', ' ', nickname) for nickname in nicknames]
+        nicknames = [nickname.strip() for nickname in nicknames]
 
-        comment_outputs = comment_model.predict_batch(comments)
-        nickname_outputs = nickname_model.predict_batch(nicknames)
+        comments = [re.sub(r'https?:\/\/[^\s]+|www.[^\s]+', '[URL]', item.comment) for item in items]
+        comments = [re.sub(r'#(\w+)', '[HASH_TAG]', comment) for comment in comments]
+        comments = [re.sub(r'[’‘]+', "'", comment) for comment in comments]
+        comments = [re.sub(r'[”“]+', '"', comment) for comment in comments]
+        comments = [re.sub(r'[\*\^]', "", comment) for comment in comments]
+        comments = [re.sub(r'\d+:(?:\d+:?)?\d+', '[TIME]', comment) for comment in comments]
+        comments = [re.sub(r'chill', '칠', comment, flags=re.IGNORECASE) for comment in comments]
+        comments = [comment.strip() for comment in comments]
+        comments = [comment or "[EMPTY]" for comment in comments]
 
+        nickname_outputs = nickname_model.predict(nicknames)
+        comment_outputs = comment_model.predict(comments)
 
         for item, comment_output, nickname_output in zip(items, comment_outputs, nickname_outputs):
             response_data.append(PredictResult(id=item.id, 
@@ -120,7 +122,7 @@ def predict_batch(data: PredictRequest):
                                                comment_predicted=comment_output))
         
         # 결과 반환
-        return PredictResponse(items=response_data)
+        return PredictResponse(items=response_data, model_type=model_type)
     
     except Exception as e:
         return {
