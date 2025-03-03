39756458b84bb95edb7300115b3a24d4f328a271
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-03 22:41:03 +0900

    tokenizer를 매 호출마다 생성하도록 수정
    - tokenizer를 매 호출마다 생성하여 비동기 처리에 용이하도록 수정
    - empty_cache 실행 타이밍 수정
    - 몇 몇 함수 위치 수정

--- helpers/transformer_classification_model.py
+++ helpers/transformer_classification_model.py
@@ -11,30 +11,25 @@ from typing import List, Union, Tuple, Dict, DefaultDict
 from collections import defaultdict
 
 class TransformerClassificationModel:
-    def __init__(self, model_type:str, base_path:str="./model"):
+    def __init__(self, model_type:str, base_model_path:str="./model"):
         self.model_type=model_type
 
-        self.model_path=os.path.join(base_path, f"{model_type}_model")
-        # self.tokenizer_path=os.path.join(base_path, f"{model_type}_tokenizer")
-        self.dataset_path=os.path.join(base_path, 'dataset.csv')
+        self.model_path=os.path.join(base_model_path, f"{model_type}_model")
+        self.tokenizer_path=os.path.join(base_model_path, f"tokenizer")
+        self.dataset_path=os.path.join(base_model_path, 'dataset.csv')
 
         self.device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         self.max_token_length=256 if model_type == "comment" else 40
 
         self.model=None
-        self.tokenizer=None
 
-    def load(self, tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast] = None):
+    def load(self):
         try:
             if self.model:
                 return
             
             self.generate_label_array()
-
-            # self._raise_file_not_fount(self.tokenizer_path)
-            # self.tokenizer=AutoTokenizer.from_pretrained(self.tokenizer_path)
-            self.tokenizer = tokenizer
             
             self._raise_file_not_fount(self.model_path)
             self.model=AutoModelForSequenceClassification.from_pretrained(self.model_path)
@@ -49,22 +44,15 @@ class TransformerClassificationModel:
     def unload(self):
         try:
             if self.model:
-                # del self.model
                 self.model=None
-            
-            # Tokenizer 교체 시 변경
-            if self.tokenizer:
-                # del self.tokenizer
-                self.tokenizer=None
-            
             gc.collect()
         
         except Exception as e:
             print(f"Error unloading model: {str(e)}")
     
-    def reload(self, tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast] = None):
+    def reload(self):
         self.unload()
-        self.load(tokenizer=tokenizer)
+        self.load()
 
     def generate_label_array(self):
         self._raise_file_not_fount(self.dataset_path)
@@ -77,8 +65,10 @@ class TransformerClassificationModel:
             raise FileNotFoundError(f"file not found at {path}")
     
     def predict(self, texts: Union[str, List[str]]) -> List[Tuple[str, float]]:
-        if self.model is None or self.tokenizer is None:
-            raise RuntimeError("Model or tokenizer not loaded")
+        if self.model is None:
+            raise RuntimeError("Model is not loaded")
+        
+        tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_path)
         
         if isinstance(texts, str):
             texts = [texts]
@@ -87,7 +77,6 @@ class TransformerClassificationModel:
 
         short_texts = []
         long_texts = []
-        grouped_data = defaultdict(list)
 
         predict_as_long_text_border_length = 300
 
@@ -101,7 +90,7 @@ class TransformerClassificationModel:
 
         def predict_short_texts(texts: List[Union[int, str]]):
             origin_indicies, batch_texts = zip(*texts)
-            tokens = self.tokenizer(list(batch_texts),
+            tokens = tokenizer(list(batch_texts),
                                     return_tensors="pt",
                                     padding=True,
                                     truncation=True,
@@ -124,13 +113,13 @@ class TransformerClassificationModel:
         # 짧은 문장도 문장별로 추론해야할까?
         for i in range(0, len(short_texts), batch_size):
             predict_short_texts(short_texts[i:i+batch_size])
-            if i % 4 * batch_size == 0:
+            if (i+1) % (4*batch_size) == 0:
                 torch.cuda.empty_cache()
 
         torch.cuda.empty_cache()
 
 
-        def predict_long_texts(inputs, local_indicies, grouped_data):
+        def predict_long_texts(inputs, local_indicies):
             def pad_tensor(tensors: List[DefaultDict[str, List[torch.Tensor]]]) -> Dict[str, torch.Tensor]:
                 keys = list(tensors[0].keys())
                 max_length = max(
@@ -142,7 +131,7 @@ class TransformerClassificationModel:
                 # 즉 left_pad는 왼쪽에 추가할 개수, right_pad는 오른쪽에 추가할 개수이다.
                 return {
                     key: torch.stack([
-                            F.pad(tensor, (0, max_length - len(tensor)), mode='constant', value=self.tokenizer.pad_token_id if key == 'input_ids' else 0) for d_dict in tensors for tensor in d_dict[key]
+                            F.pad(tensor, (0, max_length - len(tensor)), mode='constant', value=tokenizer.pad_token_id if key == 'input_ids' else 0) for d_dict in tensors for tensor in d_dict[key]
                         ]) for key in keys
                 }
 
@@ -157,30 +146,82 @@ class TransformerClassificationModel:
             for local_idx, pred_idx, prob in zip(local_indicies, predicted_class_indicies, probs):
                 grouped_data[local_idx].append([pred_idx, prob])
 
+        # 토큰 제한이 있다. 512개. 따라서 더 줄이는 방법이 필요...
+        # .?!^ 단위로 나누고 이걸 토크나이징 하는게 나을지도...?
+        def calculate_token_length(text: str):
+            def process_token(tokens, start_index: int, end_index: int):
+                result = defaultdict(list)
+                batched_length = []
+                for key in tokens:
+                    batched = tokens[key][0][start_index:end_index]
+                    batched_length.append(len(batched))
+                    result[key].append(batched)
+
+                return result, batched_length
+
+            token_collapse_length = 20
+
+            splitted_text = re.split(r'([.,!?^~]{2,}|[.!?~])(?=\s)', text)
+            merged_sentences = []
+            for i in range(0, len(splitted_text)-1, 2):
+                merged_sentences.append(splitted_text[i].strip() + splitted_text[i+1].strip())
+
+            if len(splitted_text) % 2 != 0:
+                merged_sentences.append(splitted_text[-1].strip())
+
+            tensor_result = defaultdict(list)
+            sentence_lengths = []
+
+            for sentence in merged_sentences:
+                tokens = tokenizer(sentence, return_tensors="pt")
+                token_length = len(tokens['input_ids'][0])
+
+                cur_token_idx = 0
+                while cur_token_idx < token_length:
+                    # 두 값의 차이가 {{ token_collapse_length }} 초과라면 현재 문장이 유의미한 문장임을 암시
+                    # 그것이 아니라면 유의미하지 않음 -> 유의미한 전 토큰을 가져와서 토큰화
+                    if (token_length - cur_token_idx) > token_collapse_length:
+                        local_result, batched_lengths = process_token(tokens, 
+                                                                      cur_token_idx,
+                                                                      cur_token_idx + self.max_token_length)
+                        cur_token_idx = cur_token_idx + self.max_token_length - token_collapse_length
+                    else:
+                        local_result, batched_lengths = process_token(tokens, 
+                                                                      token_length - self.max_token_length,
+                                                                      self.max_token_length)
+                        cur_token_idx = token_length # 종료조건 활성화
+                
+                    sentence_lengths.append(batched_lengths)
+                    for key in tokens:
+                        tensor_result[key].extend(local_result[key])
+
+            return tensor_result, sentence_lengths
+
         # 긴 문장은 토큰별로 추론
         local_sum = 0
         inputs = []
         local_indicies = []
+        grouped_data = defaultdict(list)
         for loop_idx, value in enumerate(long_texts):
             idx, text = value
-            tensors, tensor_lengths = self._calculate_token_length(text)
+            tensors, tensor_lengths = calculate_token_length(text)
             
             inputs.append(tensors)
             local_indicies.extend([idx] * len(tensor_lengths))
             local_sum = local_sum + len(tensor_lengths)
 
             if local_sum > batch_size:
-                predict_long_texts(inputs, local_indicies, grouped_data)
+                predict_long_texts(inputs, local_indicies)
 
                 inputs = []
                 local_indicies = []
                 local_sum = 0
 
-            if loop_idx % (batch_size // 2) == 0:
+            if (loop_idx+1) % (4*batch_size) == 0:
                 torch.cuda.empty_cache()
 
         if len(inputs) != 0:
-            predict_long_texts(inputs, local_indicies, grouped_data)
+            predict_long_texts(inputs, local_indicies)
             torch.cuda.empty_cache()
 
         for key, value in grouped_data.items():
@@ -194,68 +235,12 @@ class TransformerClassificationModel:
             
         return all_predictions, self.label_array
 
-    # 토큰 제한이 있다. 512개. 따라서 더 줄이는 방법이 필요...
-    # .?!^ 단위로 나누고 이걸 토크나이징 하는게 나을지도...?
-    def _calculate_token_length(self, text: str):
-        def process_token(tokens, start_index, end_index):
-            result = defaultdict(list)
-            batched_length = []
-            for key in tokens:
-                batched = tokens[key][0][start_index:end_index]
-                batched_length.append(len(batched))
-                result[key].append(batched)
-
-            return result, batched_length
-
-        token_collapse_length = 20
-        threshold = 20
-
-        splitted_text = re.split(r'([.,!?^~]{2,}|[.!?~])(?=\s)', text)
-        merged_sentences = []
-        for i in range(0, len(splitted_text)-1, 2):
-            merged_sentences.append(splitted_text[i].strip() + splitted_text[i+1].strip())
-        if len(splitted_text) % 2 != 0:
-            merged_sentences.append(splitted_text[-1].strip())
-
-        tensor_result = defaultdict(list)
-        sentence_lengths = []
-
-        for sentence in merged_sentences:
-            tokens = self.tokenizer(sentence, return_tensors="pt")
-            token_length = len(tokens['input_ids'][0])
-
-            cur_token_idx = 0
-            while cur_token_idx < token_length:
-                # 두 값의 차이가 {{ threshold }} 초과라면 현재 문장이 유의미한 문장임을 암시
-                # 그것이 아니라면 유의미하지 않음 -> 유의미한 전 토큰을 가져와서 토큰화
-                if (token_length - cur_token_idx) > threshold:
-                    local_result, batched_lengths = process_token(
-                        tokens, 
-                        cur_token_idx,
-                        cur_token_idx + self.max_token_length
-                    )
-                    cur_token_idx = cur_token_idx + self.max_token_length - token_collapse_length
-                else:
-                    local_result, batched_lengths = process_token(
-                        tokens, 
-                        token_length - self.max_token_length,
-                        self.max_token_length
-                    )
-                    cur_token_idx = token_length # 종료조건 활성화
-            
-                sentence_lengths.append(batched_lengths)
-                for key in tokens:
-                    tensor_result[key].extend(local_result[key])
-
-        return tensor_result, sentence_lengths
-
-
 if __name__ == "__main__":
     print('cuda available: ', torch.cuda.is_available())
 
     root_dir=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     comment_model=TransformerClassificationModel(model_type="comment",
-                                                  base_path=root_dir+'/model')
+                                                  base_model_path=root_dir+'/model')
     comment_model.load()
 
     text=input("검증할 댓글을 입력해주세요: ")
