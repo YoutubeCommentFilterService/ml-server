b81f5f25ba76579b5a2faf6c15730f7627222bc7
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-01 21:34:40 +0900

    gunicorn config 수정

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -16,9 +16,16 @@ timeout=120
 # os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-fp = os.environ.get('FP')
+fp = os.environ.get('FP', '')
+project_root_dir = os.path.dirname(os.path.abspath(__file__))
 
-def check_all_file_exists(check_file_or_dirs: list[str]) -> dict[str, bool]:
+check_file_or_dirs = ['dataset.csv', 'tokenizer']
+check_file_or_dirs.extend(
+    model + ('_fp16' if fp == 'fp16' else '') 
+    for model in ['nickname_model', 'comment_model']
+)
+
+def check_all_file_exists() -> dict[str, bool]:
     model_base_path = 'model/'
     return dict(
         map(
@@ -32,58 +39,56 @@ def is_dir_nonempty(path: str) -> bool:
     return any(p.iterdir()) if p.is_dir() else p.exists()
 
 def is_vocab_correct() -> dict[str, bool]:
-    tokenizer = AutoTokenizer.from_pretrained('model/tokenizer')
     model_subfix = '_fp16' if fp == 'fp16' else ''
-    nickname = AutoConfig.from_pretrained('model/nickname_model' + model_subfix)
-    comment = AutoConfig.from_pretrained('model/comment_model' + model_subfix)
+    nickname_vocal_len = AutoConfig.from_pretrained('model/nickname_model' + model_subfix).vocab_size
+    comment_vocal_len = AutoConfig.from_pretrained('model/comment_model' + model_subfix).vocab_size
+    tokenizer_vocal_len = len(AutoTokenizer.from_pretrained('model/tokenizer'))
 
     return {
-        'nickname_model' + model_subfix: len(tokenizer) == nickname.vocab_size,
-        'comment_model' + model_subfix : len(tokenizer) == comment.vocab_size
+        'nickname_model' + model_subfix: tokenizer_vocal_len == nickname_vocal_len,
+        'comment_model' + model_subfix : tokenizer_vocal_len == comment_vocal_len
     }
 
-def when_ready(server):
-    project_root_dir = os.path.dirname(os.path.abspath(__file__))
-
-    load_dotenv(os.path.join(project_root_dir, "env", ".env"))
-
+def download_neccessary_files():
     print('Download Neccesarry Files Start...', flush=True)
     helper = S3Helper(project_root_dir, 'youtube-comment-predict')
-    if not is_exist_model():
-        helper.download()
-    else:
+    try:
+        check_all_neccessary_file_exists()
         helper.download(['dataset.csv'])
+    except:
+        # 모든 파일 다운로드
+        helper.download()
     print('Download Neccesarry Files Finished!', flush=True)
 
-    check_file_or_dirs = ['dataset.csv', 'tokenizer']
-    check_file_or_dirs.extend(
-        model + ('_fp16' if fp == 'fp16' else '') 
-        for model in ['nickname_model', 'comment_model']
-    )
-
-    missing_files = [ k for k, v in check_all_file_exists(check_file_or_dirs).items() if not v ]
+def check_all_neccessary_file_exists():
+    missing_files = [ k for k, v in check_all_file_exists().items() if not v ]
     if missing_files:
         raise RuntimeError(f'필요한 모델/파일이 없습니다: {missing_files}')
     
+def check_vocab_is_correct():
     incorrect_vocab = [ k for k, v in is_vocab_correct().items() if not v ]
     if incorrect_vocab:
         raise RuntimeError(f'해당 model의 vocab_size가 올바르지 않습니다: {incorrect_vocab}')
-
+    
+def start_redis():
     print('Loading Redis Client Start...', flush=True)
     client = Redis()
-    file_hash = get_model_version_hash()
-    client.set(REDIS_MODEL_VERSION_KEY, file_hash)
+    try:
+        stat = os.stat('./model/dataset.csv')
+        key = f"{stat.st_mtime}-{stat.st_size}".encode("utf-8")
+        file_hash = hashlib.md5(key).hexdigest()
+        client.set(REDIS_MODEL_VERSION_KEY, file_hash)
+    finally:
+        client.close()
     print('Loading Redis Client Finished!', flush=True)
 
-    print('master ready!')
+def on_starting(server):
+    load_dotenv(os.path.join(project_root_dir, "env", ".env"))
 
-def is_exist_model():
-    if not os.path.exists('./model'):
-        return False
-    files = [re.sub(r'_[a-z0-9]+', '', f) for f in os.listdir('./model') if f != 'dataset.csv']
-    return len(set(files)) == 3
+    download_neccessary_files()
 
-def get_model_version_hash():
-    stat = os.stat('./model/dataset.csv')
-    key = f"{stat.st_mtime}-{stat.st_size}".encode("utf-8")
-    return hashlib.md5(key).hexdigest()
+    check_vocab_is_correct()
+    start_redis()
+
+    print("server started!")
+    pass
